f404bd85e8086e7db8e5dd31d4a5b562467f0c8b
Clariveljn <cjeldres5@gmail.com>
2023-01-09 18:09:46 -0300

    Agregue página nosotros

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,10 +1,50 @@
+import imagen from '../../public/img/nosotros.jpg'
+import styles from '~/styles/nosotros.css'
 
+export function meta() {
+  return {
+    title: 'GuitarLA - Sobre Nosotros',
+    description: 'Venta de guitaras, bog de música'
+  }
+}
 
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as: 'image'
+    }
+  ]
+}
 function Nosotros() {
   return (
-    <div>
-      Nosotros
-    </div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className="contenido">
+        <img src={imagen} alt="imagen sobre nosotros" />
+        <div>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+          ornare neque ut urna tempus, eget rhoncus libero feugiat. Morbi id 
+          egestas quam. Sed sollicitudin accumsan lectus imperdiet iaculis. 
+          Mauris luctus tempor ipsum at vehicula
+          </p>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+          ornare neque ut urna tempus, eget rhoncus libero feugiat. Morbi id 
+          egestas quam. Sed sollicitudin accumsan lectus imperdiet iaculis. 
+          Mauris luctus tempor ipsum at vehicula
+          </p>
+        </div>
+
+      </div>
+    </main>
   )
 }
 
