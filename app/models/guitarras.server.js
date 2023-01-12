c8bb109b5b332483ec5e90eb874e3031f69a4143
Clariveljn <cjeldres5@gmail.com>
2023-01-12 15:48:34 -0300

    Agregue Blog

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -2,8 +2,7 @@ export async function getGuitarras() {
   const respuesta = await fetch(
     `${process.env.API_URL}/guitarras?populate=imagen`
   );
-  const resultado = await respuesta.json();
-  return resultado;
+  return await respuesta.json();
 }
 
 export async function getGuitarra(url) {
--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,10 +1,44 @@
+import { useLoaderData } from "@remix-run/react"
+import Post from '~/components/post'
+import { getPosts } from "~/models/posts.server"
+import styles from '~/styles/blog.css'
 
+export function meta() {
+  return {
+    title: 'GuitarLA - Nuestro Blog',
+    description: 'GuitarLA, Blog de música y venta de guitarras'
+  }
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async function loader() {
+  const posts = await getPosts()
+  return posts.data
+}
 
 function Blog() {
+  const post = useLoaderData()
   return (
-    <div>
-      Blog
-    </div>
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+          {post.map(post => (
+            <Post
+              key={post.id}
+              post={post.attributes}            
+            />
+          ))}
+      </div>
+      
+    </main>
   )
 }
 
